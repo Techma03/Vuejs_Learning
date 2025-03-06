1a3d70238dceef68d4be12b4170fbd4b73b04583
princedev064 <princedeveloper064@gmail.com>
2025-03-06 13:34:08 +0100

    destruction d'une instance success

--- episode_02/app.js
+++ episode_02/app.js
@@ -1,16 +1,32 @@
 let vm = new Vue({
-    el: "#app",
+    el: '#app',
     data: {
-        message: 'salut les gens',
-        link: 'https://techma03.com',
-        success: true,
-        persons: ['Jonathan', 'Aurel', 'Prince', 'Jilber', 'Henry']
+        seconds: 0
     },
-    methods: {
-        addPerson: function(){
-            this.persons.push('Moses')
-        }
-
+    mounted: function () {
+        this.$interval = setInterval(() => {
+            console.log('Time')
+            this.seconds++
+        }, 1000)
+    },
+    destroyed: function () {
+        clearInterval(this.$interval)
     }
-
 })
+
+// let vm = new Vue({
+//     el: "#app",
+//     data: {
+//         message: 'salut les gens',
+//         link: 'https://techma03.com',
+//         success: true,
+//         persons: ['Jonathan', 'Aurel', 'Prince', 'Jilber', 'Henry']
+//     },
+//     methods: {
+//         addPerson: function(){
+//             this.persons.push('Moses')
+//         }
+
+//     }
+
+// })
