2948944a401dbdd5116059ed4af1b74f2d1f8c40
princedev064 <princedeveloper064@gmail.com>
2025-03-06 12:15:34 +0100

    Attribut Basic VueJs

--- episode_01/app.js
+++ episode_01/app.js
@@ -2,7 +2,15 @@ new Vue({
     el: "#app",
     data:{
         message: 'salut les gens',
-        link: 'https://techma03.com'
+        link: 'https://techma03.com',
+        success: true,
+        persons:['Jonathan','Aurel','Prince','Jilber','Henry']
     },
+    methods:{
+        close: function(){
+            this.message="Fermé"
+            this.success = false
+        }
+    }
     
 })
